819266b8524c7456ea031d4e742fc34f04d555f0
aryaman <thediabloxtreme666@gmail.com>
2024-10-22 22:15:10 +0530

    [WIP] Working on server start capabilities. Currently unorganized in main.py with plans to clean up later on.

--- exaroton-api-client/main.py
+++ exaroton-api-client/main.py
@@ -1,1 +1,13 @@
 from exaroton_api_client import AuthenticatedClient as AC
+from exaroton_api_client.api.server_actions import get_start_server as startServer
+from exaroton_api_client.models.server import Server
+import json
+
+key = ""
+with open("config.json", "r") as file:
+    file = json.load(file)
+    key = file['key']
+
+client = AC(base_url="https://api.exaroton.com/v1/", token=key)
+
+
